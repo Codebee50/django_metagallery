2c1f43b283652e6c2d8059bdcc8a6d040f8d58a2
Code bee <codebee345@outlook.com>
2025-02-24 11:21:36 +0100

    added csrf origins

--- metagallery/settings.py
+++ metagallery/settings.py
@@ -197,3 +197,6 @@ EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['https://metagallery.up.railway.app/']
