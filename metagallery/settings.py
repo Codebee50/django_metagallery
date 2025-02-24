d05ed8d9717a1ed44a82609966d9c032eed15e23
Code bee <codebee345@outlook.com>
2025-02-24 11:48:38 +0100

    added aws s3 configurtions

--- metagallery/settings.py
+++ metagallery/settings.py
@@ -200,3 +200,31 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://metagallery.up.railway.app']
+    
+
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    AWS_S3_FILE_OVERWRITE = False
+
+    STORAGES = {
+        # Media file (image) management   
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            "LOCATION": 'media/'
+        },
+        
+        # CSS and JS file management
+        # "staticfiles": {
+        #     "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        # },
+
+    }
+
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
